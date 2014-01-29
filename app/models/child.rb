cbd66ede4e776bcbf4c0a0f87d21af582ef538de
Apte Tanuj <tva@andrew.cmu.edu>
2014-01-29 01:32:37 -0500

    added method and scopes to Child

--- app/models/child.rb
+++ app/models/child.rb
@@ -4,5 +4,11 @@ class Child < ActiveRecord::Base
   has_many :chores
 
   validates_presence_of :first_name, :last_name
-  
+
+  scope :alphabetical, order('last_name', 'first_name')
+  scope :active, where ('active = ?', true)
+
+  def name
+  return self.first_name + " " + self.last_name
+end
 end
