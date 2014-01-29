b10dbbdbfcebc76743ef2eb8c0cfb15f1863fcb8
Apte Tanuj <tva@andrew.cmu.edu>
2014-01-29 01:40:01 -0500

    Fixed Task model errors

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ActiveRecord::Base
   attr_accessible :active, :name, :points
   
+  has_many :chores
+  validates_presence_of :name, :points
+  validates_numericality_of :points, :only_integer => true, :greater_than => 0
+
+  scope :alphabetical, order('name')
+  scope :active, where('active = ?', true)
 end
